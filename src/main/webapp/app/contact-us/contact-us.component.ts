6fcc30fa87825adfc545c6b44a80862ff3bcccba
Jemal <jemal@zoomdata.com>
2019-11-01 14:51:44 -0400

    clean up

--- src/main/webapp/app/contact-us/contact-us.component.ts
+++ src/main/webapp/app/contact-us/contact-us.component.ts
@@ -1,42 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
-import { ErrorMessage } from 'ng-bootstrap-form-validation/lib/Models/error-message';
 
 @Component({
   selector: 'jhi-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['contact-us.component.scss']
+  styleUrls: ['contact-us.scss']
 })
 export class ContactUsComponent implements OnInit {
-  formGroup: FormGroup;
+  ngOnInit() {}
 
-  customErrorMessages: ErrorMessage[] = [
-    {
-      error: 'required',
-      format: (label, error) => `${label.toUpperCase()} IS DEFINITELY REQUIRED!`
-    },
-    {
-      error: 'pattern',
-      format: (label, error) => `${label.toUpperCase()} DOESN'T LOOK RIGHT...`
-    }
-  ];
+  onSubmit() {}
 
-  ngOnInit() {
-    this.formGroup = new FormGroup({
-      Email: new FormControl('', [
-        Validators.required,
-        Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
-      ]),
-      Password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(20)])
-    });
-  }
-
-  onSubmit() {
-    console.log(this.formGroup);
-  }
-
-  onReset() {
-    this.formGroup.reset();
-  }
+  onReset() {}
 }
